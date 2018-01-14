049d78a4b8ca3583e2d6dcfd384e4d0f36590ebb
Perry <chenzhipeng901105@gmail.com>
2018-01-14 19:56:10 +0800

    add code

--- tour/JS/EventHandler.js
+++ tour/JS/EventHandler.js
@@ -1,17 +1,33 @@
-AFRAME.registerComponent('menuVisible', {
+AFRAME.registerComponent('hello', {
   schema: {
-    eventN: {type: 'string', default: ''}
+    event: {type: 'string', default: ''},
+    message: {type: 'string', default: ''}
   },
+
+  multiple: true,
 
   init: function () {
     var self = this;
-    // this.eventHandlerFn = function () { console.log(self.data.eventN); }
-    // console.log(this.data.eventN);
-    console.log('event component init');
+    this.eventHandlerFn = function () { console.log(self.data.message); }
   },
 
   update: function () {
-    console.log('update');
+
+    var data = this.data
+    var el = this.el
+
+    if (data.event) {
+      el.addEventListener(data.event, function () {
+        var sceneEl = document.querySelector('a-scene');
+        var menu = sceneEl.querySelector('#links');
+        menu.setAttribute('visible', data.message);
+
+        console.log(data.message)
+      });
+    } else {
+
+    }
+    console.log(this.id);
   }
 });
 
